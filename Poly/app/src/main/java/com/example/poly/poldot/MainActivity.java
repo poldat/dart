cbba4ceb953e78ea96a53b3259a48c20fed17b5c
PY-KHR <38966197+PY-KHR@users.noreply.github.com>
2018-09-04 12:23:28 +0900

    test

--- Poly/app/src/main/java/com/example/poly/poldot/MainActivity.java
+++ Poly/app/src/main/java/com/example/poly/poldot/MainActivity.java
@@ -8,6 +8,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.game_result);
+        setContentView(R.layout.lobby);
     }
+
+
 }
